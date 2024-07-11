9089b6ff1723e1202822ce8df9dc5acfcac6a4bd
gexu13 <gexu13@outlook.com>
2024-07-11 00:35:38 -0700

    add review object to reviewSchema

--- validation/reviewSchema.js
+++ validation/reviewSchema.js
@@ -1,8 +1,10 @@
 const Joi = require('joi');
 
 const reviewSchema = Joi.object({
-    body: Joi.string().required(),
-    rating: Joi.number().required().min(1).max(5).integer(),
-}).required();
+    review: Joi.object({
+        body: Joi.string().required(),
+        rating: Joi.number().required().min(1).max(5).integer(),
+    }).required()
+})
 
 module.exports = reviewSchema;
