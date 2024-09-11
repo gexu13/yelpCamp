426befe223630df1e8937ea349d82ceb59a6bebb
gexu13 <gexu13@outlook.com>
2024-09-11 15:15:22 -0700

    add checkAuthor function to require permission

--- routes/campgroundRoutes.js
+++ routes/campgroundRoutes.js
@@ -16,6 +16,18 @@ const validateCampground = (req, res, next) => {
     }
     next();
 }
+
+const checkAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
+    // check if the author of the campground is the current user
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', "You do not have permission!")
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next()
+}
+
 /***** CAMPGROUND ROUTES *****/
 // index route
 router.get('/', async (req, res, next) => {
@@ -37,6 +49,7 @@ router.post('/', requireLogin, validateCampground, async (req, res, next) => {
     try {
         // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
         const campground = new Campground(req.body.campground);
+        // set the current user as the author of the new campground
         campground.author = req.user._id
         await campground.save();
         req.flash('success', 'Successfully made a new campground!');
@@ -64,7 +77,7 @@ router.get('/:id', async (req, res, next) => {
 })
 
 // get campground edit form route
-router.get('/:id/edit', requireLogin, async (req, res, next) => {
+router.get('/:id/edit', requireLogin, checkAuthor, async (req, res, next) => {
     try {
         const { id } = req.params;
         const campground = await Campground.findById(id);
@@ -80,10 +93,9 @@ router.get('/:id/edit', requireLogin, async (req, res, next) => {
 })
 
 // patch (update and edit) route
-router.patch('/:id', requireLogin, validateCampground, async (req, res, next) => {
+router.patch('/:id', requireLogin, checkAuthor, validateCampground, async (req, res, next) => {
     try {
         const { id } = req.params;
-        
         const updates = req.body.campground;
         const campground = await Campground.findByIdAndUpdate(id, updates, {runValidators: true});
         req.flash('success', 'Successfully updated the campground!');
@@ -94,7 +106,7 @@ router.patch('/:id', requireLogin, validateCampground, async (req, res, next) =>
 }) 
 
 // delete route
-router.delete('/:id', requireLogin, async (req, res, next) => {
+router.delete('/:id', requireLogin, checkAuthor, async (req, res, next) => {
     try {
         const { id } = req.params;
         const campground = await Campground.findByIdAndDelete(id);
