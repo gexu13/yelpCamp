1d41dbc6a325d74caede7d8846d41f961da95ad4
gexu13 <gexu13@outlook.com>
2024-09-21 00:11:30 -0700

    add multer and cloudinary so that user can upload image and store it on cloudinary

--- routes/campgroundRoutes.js
+++ routes/campgroundRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { requireLogin, validateCampground, checkAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds')
+const multer = require('multer')
+const { storage } = require('../cloudinary')
+const upload = multer({ storage })
 
 /***** CAMPGROUND ROUTES *****/
 // index route
@@ -11,7 +14,13 @@ router.get('/', campgrounds.index)
 router.get('/new', requireLogin, campgrounds.getNewForm)
 
 // post route
-router.post('/', requireLogin, validateCampground, campgrounds.postNewCampground)
+// router.post('/', requireLogin, validateCampground, campgrounds.postNewCampground)
+router.post('/', upload.array('image'), (req, res) => {
+    console.log(req.body)
+    console.log(req.files)
+    res.send('It worked!')
+})
+
 
 // show route
 router.get('/:id', campgrounds.showCampground)
